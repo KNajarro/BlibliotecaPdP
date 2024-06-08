05a09775de699a6c34ae6bcc6e1c0cc7cdb611ef
Kevin Manuel Lemus <nkevinmanuel94@gmail.com>
2024-06-07 23:51:11 -0600

    Fixes

--- obj/Debug/net6.0/BibliotecaBlazor.AssemblyInfo.cs
+++ obj/Debug/net6.0/BibliotecaBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e687c8386031a0a7102ad45a45487670416bf20e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
